e098db68b7c48ccfbf552ab036bfcfab80108489
Shaktilar29 <gmz.ag@outlook.com>
2017-02-07 17:07:27 +0100

    Delete Bin

--- src/com/iup/tp/twitup/ihm/vue/swing/ISwingView.java
+++ src/com/iup/tp/twitup/ihm/vue/swing/ISwingView.java
@@ -2,7 +2,9 @@ package com.iup.tp.twitup.ihm.vue.swing;
 
 import javax.swing.JComponent;
 
-public interface ISwingView {
+import com.iup.tp.twitup.ihm.IView;
+
+public interface ISwingView extends IView{
 
 	public JComponent showView();
 }
